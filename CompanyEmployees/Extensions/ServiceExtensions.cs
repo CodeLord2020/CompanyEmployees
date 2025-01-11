ce2b2ea7c6413ff26a794a9870e6df75fb91882c
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-11 11:53:41 +0100

    pre endpoinnts controller wrap

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
 using Repository;
+using Service;
+using Microsoft.EntityFrameworkCore;
+using Service.Contracts;
 
 namespace CompanyEmployees.Extensions;
 
@@ -15,6 +18,13 @@ public static class ServiceExtensions
 			.AllowAnyHeader());
 		});
 
+	public static void ConfigureSqlContext(this IServiceCollection services,
+		IConfiguration configuration) => services.AddDbContext<RepositoryContext>(opts =>
+			opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+	public static void ConfigureServiceManager (this IServiceCollection services) =>
+		services.AddScoped<IServiceManager, ServiceManager>();
+	
 	public static void ConfigureRepositoryManager(this IServiceCollection services) =>
 		services.AddScoped<IRepositoryManager, RepositoryManager>();
 
--- CompanyEmployees/obj/Debug/net8.0/CompanyEmployees.AssemblyInfo.cs
+++ CompanyEmployees/obj/Debug/net8.0/CompanyEmployees.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89745a06ae2d547cfcc1651040e492ddc41de88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4240502a2b61d8a39bb9e61cc5d332ebb939b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

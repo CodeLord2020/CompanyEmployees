9ba0ad3dcc4e04f17e3e491ee1000b2b136f6864
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-11 11:11:23 +0100

    service manager

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2dea8bfffa1c82558d60e5fbdafba18faf7161")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fac3e5888ae6a149f5b33e5863db6bc747c55a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

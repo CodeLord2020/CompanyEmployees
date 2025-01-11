b98ed0c52f79844ac741672d5c6546ce813baea2
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-11 18:04:25 +0100

    Companiescontrollers

--- CompanyEmployees.Presentation/obj/Debug/net8.0/CompanyEmployees.Presentation.AssemblyInfo.cs
+++ CompanyEmployees.Presentation/obj/Debug/net8.0/CompanyEmployees.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd1d910ed5d69cd9a08a71faec4056043f16784")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bda2a78afc56aa032a1fea48989907cff4d0361")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

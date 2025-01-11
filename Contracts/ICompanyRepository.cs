1e0262a47a451979bc1b86d1dcaa750554a8b3c2
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-11 18:05:05 +0100

    Companiescontrollers interface update

--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Entities.Models;
 
-namespace Contracts
+namespace Contracts;
+
+public interface ICompanyRepository
 {
-    public interface ICompanyRepository
-    {
-        
-    }
+    IEnumerable<Company> GetAllCompanies(bool trackChanges);
+    Company GetCompany(Guid companyId, bool trackChanges);
+    void CreateCompany(Company company);
+    IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
 }

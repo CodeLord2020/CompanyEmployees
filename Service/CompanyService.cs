e51c7e21bc244c5f9212436fd3c3b9541aa33606
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-11 11:22:33 +0100

    Company service logic

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contracts;
+using Service.Contracts;
 
 namespace Service
 {
-    public class CompanyService
+    internal sealed class CompanyService : ICompanyService
     {
-        
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
     }
 }

d8794d2953fe7f28ae02833c5eeb10c454ea6291
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-11 18:05:29 +0100

    Companiescontrollers repo update

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -8,5 +8,24 @@ namespace Repository
         public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
         {
         }
+
+        public void CreateCompany(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+           FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+        
+
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company GetCompany(Guid companyId, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

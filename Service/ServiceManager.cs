4d4240502a2b61d8a39bb9e61cc5d332ebb939b2
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-11 11:19:13 +0100

    service manager logic

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contracts;
+using Service.Contracts;
 
 namespace Service
 {
-    public class ServiceManager
+    public sealed class ServiceManager : IServiceManager
     {
-        
+        private readonly Lazy<ICompanyService> _companyService;
+        private readonly Lazy<IEmployeeService> _employeeService;
+
+        public ServiceManager(IRepositoryManager repositoryManager, ILoggerManager logger)
+        {
+            _companyService = new Lazy<ICompanyService>(() => new CompanyService(repositoryManager, logger));
+            _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(repositoryManager, logger));
+        }
+        public ICompanyService CompanyService => throw new NotImplementedException();
+
+        public IEmployeeService EmployeeService => throw new NotImplementedException();
     }
 }

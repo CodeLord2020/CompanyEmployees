8a2441757a948486b245df0b58061cae93b7e53c
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-13 20:59:25 +0100

    employees by id&comID

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -3,6 +3,7 @@ namespace Service.Contracts
 {
     public interface IEmployeeService
     {
-        
+        IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
+        EmployeeDto GetEmployee(Guid employeeId, Guid companyId, bool trackChanges);   
     }
 }
--- Service.Contracts/obj/Debug/net8.0/Service.Contracts.AssemblyInfo.cs
+++ Service.Contracts/obj/Debug/net8.0/Service.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b934e6b942cc8bd96ed341b270288dd311a032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b0f5e552b422521ef270fe32096c071b4d8a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

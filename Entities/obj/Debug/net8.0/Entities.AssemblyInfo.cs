657fb6faba9794c214a6199b8f12aaf2d32382d5
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-13 20:53:05 +0100

    NotFound Exceptiion employees

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305b0ed059e14dc0ca99f289b0b9c4eca1335255")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b0f5e552b422521ef270fe32096c071b4d8a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

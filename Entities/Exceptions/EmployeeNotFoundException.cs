328542c3a628c33d54f09fb381d084b0ce542b21
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-13 21:03:28 +0100

    NotFound Exceptiion employees

--- Entities/Exceptions/EmployeeNotFoundException.cs
+++ Entities/Exceptions/EmployeeNotFoundException.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 
 namespace Entities.Exceptions
 {
-    public class EmployeeNotFoundException
+    public class EmployeeNotFoundException : NotFoundException
     {
-        
+        public EmployeeNotFoundException(Guid employeeId) :
+         base($"Employee with id: {employeeId} doesn't exist in the database.")
+        {
+        }
     }
 }

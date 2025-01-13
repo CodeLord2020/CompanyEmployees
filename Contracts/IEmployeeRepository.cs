60b1267cea0f661797d962f1e0c0a9393020f6ac
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-13 21:02:39 +0100

    Irepo employees by id&comID

--- Contracts/IEmployeeRepository.cs
+++ Contracts/IEmployeeRepository.cs
@@ -9,5 +9,6 @@ namespace Contracts
     public interface IEmployeeRepository
     {
         IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
+        Employee GetEmployee(Guid employeeId, Guid companyId, bool trackChanges);
     }
 }

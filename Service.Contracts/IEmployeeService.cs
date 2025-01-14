b09b7dc5ea1dc542e0873d2f92b1d9e5b92505db
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-14 13:16:55 +0100

    create compEmp serviceI

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -1,9 +1,12 @@
 
+using Shared.DataTransferObjects;
+
 namespace Service.Contracts
 {
     public interface IEmployeeService
     {
         IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
         EmployeeDto GetEmployee(Guid employeeId, Guid companyId, bool trackChanges);   
+        EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employee, bool trackChanges);
     }
 }

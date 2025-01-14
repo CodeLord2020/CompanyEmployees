62c341e51489c0ceb633722293c92df73973e0c3
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-14 20:57:17 +0100

    badrequest exceptions

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e252ebf02fbcbce7a7afbd4dfaa4270b2224f8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5e741fb4fcc0f44665635f093da8135444749d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f749422d7fb30afa1a47942efdf8d3e62ffdb9b1
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-14 20:37:16 +0100

    comp byIds

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -19,10 +19,8 @@ namespace Repository
            FindAll(trackChanges).OrderBy(c => c.Name).ToList();
         
 
-        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges)
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(c => ids.Contains(c.Id), trackChanges).ToList();
 
         public Company GetCompany(Guid companyId, bool trackChanges) 
         {

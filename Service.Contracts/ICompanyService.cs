5b5e741fb4fcc0f44665635f093da8135444749d
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-14 20:40:27 +0100

    compByIds serviceI

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -7,4 +7,6 @@ public interface ICompanyService
     IEnumerable<CompanyDTO> GetAllCompanies(bool trackChanges);
     CompanyDTO GetCompany(Guid companyId, bool trackChanges);
     CompanyDTO CreateCompany(CompanyForCreationDto company);
+    IEnumerable<CompanyDTO> GetCompaniesById(IEnumerable<Guid> Ids, bool trackChanges);
+
 }

1586eecb0284de006ce6842ae1d123633598df3f
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-15 13:01:39 +0100

    del compEmp Iservice

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -8,5 +8,6 @@ namespace Service.Contracts
         IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
         EmployeeDto GetEmployee(Guid employeeId, Guid companyId, bool trackChanges);   
         EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employee, bool trackChanges);
+        void DeleteEmployee(Guid employeeId, Guid companyId, bool trackChanges);
     }
 }

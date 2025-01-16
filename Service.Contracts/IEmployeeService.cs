a150a05bf299136211956d2550910ac31247459c
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-16 14:26:39 +0100

    update emp  Iservice

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -5,9 +5,11 @@ namespace Service.Contracts
 {
     public interface IEmployeeService
     {
+        IEnumerable<EmployeeDto> GetAllEmployees(bool trackChanges);
         IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges);
         EmployeeDto GetEmployee(Guid employeeId, Guid companyId, bool trackChanges);   
         EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employee, bool trackChanges);
         void DeleteEmployee(Guid employeeId, Guid companyId, bool trackChanges);
+        void UpdateEmployeeForCompany(Guid companyId, Guid employeeId, EmployeeForUpdateDto employeeForUpdate, bool compTrackChanges, bool empTrackChanges);
     }
 }

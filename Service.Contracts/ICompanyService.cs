e6f01c8e9ffbc27afb3f14ba4f61fdc519ab104d
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-19 17:49:05 +0100

    comp patch Iservice

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -12,6 +12,6 @@ public interface ICompanyService
     Task<(IEnumerable<CompanyDTO> companies, string Ids)> CreateCompanyByBulkAsync(IEnumerable<CompanyForCreationDto> companyCollection);
     Task DeleteCompanyAsync(Guid companyId, bool trackChanges);
     Task UpdateCompanyAsync(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
-    (CompanyForUpdateDto companyToPatch, Company companyEntity) GetCompanyForPatch(
+    (CompanyForUpdateDto companyToPatch, Company companyEntity) GetCompanyForPatchAsync(
         Guid companyId, bool compTrackChanges);
 }

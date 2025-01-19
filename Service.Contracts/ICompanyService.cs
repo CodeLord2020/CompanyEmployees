c885a744f4a022e5cacfacd9cadb07c8158aa91b
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-19 14:41:37 +0100

    emp patch Iservice to async

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -4,12 +4,12 @@ namespace Service.Contracts;
 
 public interface ICompanyService
 {
-    IEnumerable<CompanyDTO> GetAllCompanies(bool trackChanges);
-    CompanyDTO GetCompany(Guid companyId, bool trackChanges);
-    CompanyDTO CreateCompany(CompanyForCreationDto company);
-    IEnumerable<CompanyDTO> GetCompaniesById(IEnumerable<Guid> Ids, bool trackChanges);
-    (IEnumerable<CompanyDTO> companies, string Ids) CreateCompanyByBulk(IEnumerable<CompanyForCreationDto> companyCollection);
-    void DeleteCompany(Guid companyId, bool trackChanges);
-    void UpdateCompany(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
+    Task <IEnumerable<CompanyDTO>> GetAllCompaniesAsync(bool trackChanges);
+    Task <CompanyDTO> GetCompanyAsync(Guid companyId, bool trackChanges);
+    Task <CompanyDTO> CreateCompanyAsync(CompanyForCreationDto company);
+    Task <IEnumerable<CompanyDTO>> GetCompaniesByIdsAsync(IEnumerable<Guid> Ids, bool trackChanges);
+    Task <(IEnumerable<CompanyDTO> companies, string Ids)> CreateCompanyByBulkAsync(IEnumerable<CompanyForCreationDto> companyCollection);
+    Task DeleteCompanyAsync(Guid companyId, bool trackChanges);
+    Task UpdateCompanyAsync(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
 
 }

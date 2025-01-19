85de53e9ae576c033d200a3b25675c53d16b3670
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-19 21:53:00 +0100

    comp repo to async

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -2,6 +2,7 @@
 using Contracts;
 using Entities.Exceptions;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
@@ -28,19 +29,32 @@ namespace Repository
           return FindAll(trackChanges).OrderBy(e => e.Name).ToList();
         }
 
+        public async Task<IEnumerable<Employee>> GetAllEmployeesAsync(bool trackChanges)
+        {
+            return await FindAll(trackChanges).OrderBy(e => e.Name).ToListAsync();
+        }
+
         public Employee GetEmployee(Guid employeeId, Guid companyId, bool trackChanges)
         {
-            var employee = FindByCondition(e => e.Id == employeeId && e.CompanyId == companyId,  trackChanges).SingleOrDefault();
-            if (employee is null)
-            {
-                throw new EmployeeNotFoundException(employeeId);
-            }
+            var employee = FindByCondition(e => e.Id == employeeId && e.CompanyId == companyId,  trackChanges).SingleOrDefault()
+            ?? throw new EmployeeNotFoundException(employeeId);
             return employee;
+        }
+
+        public async Task<Employee> GetEmployeeAsync(Guid employeeId, Guid companyId, bool trackChanges)
+        {
+            return await FindByCondition(e => e.Id == employeeId && e.CompanyId == companyId,  trackChanges).SingleOrDefaultAsync() 
+            ?? throw new EmployeeNotFoundException(employeeId);
         }
 
         public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
         
-            FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).OrderBy(e => e.Name).ToList(); 
-        
+            FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).OrderBy(e => e.Name).ToList();
+            // [.. FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).OrderBy(e => e.Name)];
+
+        public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges)
+        {
+            return await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).ToListAsync();   
+        }
     }
 }

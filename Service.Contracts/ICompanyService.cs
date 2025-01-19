37f5731f3ba0984ccbcad8fbec749bda4f98b36f
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-19 14:47:33 +0100

    comp patch Iservice to async

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -7,9 +7,9 @@ public interface ICompanyService
     Task <IEnumerable<CompanyDTO>> GetAllCompaniesAsync(bool trackChanges);
     Task <CompanyDTO> GetCompanyAsync(Guid companyId, bool trackChanges);
     Task <CompanyDTO> CreateCompanyAsync(CompanyForCreationDto company);
-    Task <IEnumerable<CompanyDTO>> GetCompaniesByIdsAsync(IEnumerable<Guid> Ids, bool trackChanges);
+    Task <IEnumerable<CompanyDTO>> GetCompaniesByIdAsync(IEnumerable<Guid> Ids, bool trackChanges);
     Task <(IEnumerable<CompanyDTO> companies, string Ids)> CreateCompanyByBulkAsync(IEnumerable<CompanyForCreationDto> companyCollection);
-    Task DeleteCompanyAsync(Guid companyId, bool trackChanges);
-    Task UpdateCompanyAsync(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
+    void DeleteCompanyAsync(Guid companyId, bool trackChanges);
+    void UpdateCompanyAsync(Guid companyid, CompanyForUpdateDto companyForUpdate, bool trackChanges);
 
 }

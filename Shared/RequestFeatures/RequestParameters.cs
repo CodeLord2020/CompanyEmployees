d3fb1ba7b8846140994a2cea76671d0cc1e34b9e
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-26 14:42:46 +0100

    page logic to RequestParameters

--- Shared/RequestFeatures/RequestParameters.cs
+++ Shared/RequestFeatures/RequestParameters.cs
@@ -5,8 +5,20 @@ using System.Threading.Tasks;
 
 namespace Shared.RequestFeatures
 {
-    public class RequestParameters
+    public abstract class RequestParameters
     {
-        
+        const int maxPageSize = 50;
+        public int pageNumber {get; set;} = 1;
+
+        private int _pageSize = 10;
+        public int PageSize {
+            get {
+                 return _pageSize;
+                }
+
+            set{
+                 _pageSize = (value > maxPageSize) ? maxPageSize : value;
+                }
+            }
     }
 }

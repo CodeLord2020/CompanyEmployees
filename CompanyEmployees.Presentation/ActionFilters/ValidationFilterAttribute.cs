551ddda46f717e90d63dc6b406d9727ff54bf60a
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-26 14:06:02 +0100

    ValidationFilterAttribute

--- CompanyEmployees.Presentation/ActionFilters/ValidationFilterAttribute.cs
+++ CompanyEmployees.Presentation/ActionFilters/ValidationFilterAttribute.cs
@@ -2,11 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace CompanyEmployees.Presentation.ActionFilters
 {
-    public class ValidationFilterAttribute
+    public class ValidationFilterAttribute : IActionFilter
     {
-        
+        public ValidationFilterAttribute()
+        {
+            
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var action = context.RouteData.Values["action"];
+            var controller = context.RouteData.Values["controller"];
+
+            var param = context.ActionArguments.SingleOrDefault
+            (a => a.Value.ToString().ToLower().Contains("Dto")).Value;
+            
+            if (param is null)
+             {
+                context.Result = new BadRequestObjectResult ($"Object is null. Controller:{controller}, action: {action}");
+                return;
+            }
+
+            if (!context.ModelState.IsValid){
+                context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+            }
+        }
     }
 }

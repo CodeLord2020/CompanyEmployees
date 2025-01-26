0258b9a82cbcb8ece969c234d3548850d4eb13e1
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-26 14:42:12 +0100

    RequestParameters

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551ddda46f717e90d63dc6b406d9727ff54bf60a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5d9482818d753a75742ba43485eae87845d2ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

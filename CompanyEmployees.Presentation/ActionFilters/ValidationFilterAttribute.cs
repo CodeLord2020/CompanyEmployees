82e4f369e7d8ad875ac22a08671c762bd1027938
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-26 13:48:56 +0100

    ActionFilters

---
+++ CompanyEmployees.Presentation/ActionFilters/ValidationFilterAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.ActionFilters
+{
+    public class ValidationFilterAttribute
+    {
+        
+    }
+}
--- LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
+++ LoggerService/obj/Debug/net8.0/LoggerService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfac583815f39c892d47bee36c4072c911655806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3afe60d6cef2b497b28221e53a8699947bebba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

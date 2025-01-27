fe2fbd456f981c7598f126be273aca141fd0f031
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-27 10:44:14 +0100

    add RepositoryEmployeeExtensions logic

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -2,11 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entities.Models;
 
 namespace Repository.Extensions
 {
-    public class RepositoryEmployeeExtensions
+    public static class RepositoryEmployeeExtensions
     {
-        
+        public static IQueryable<Employee> FilterEmployees(this IQueryable<Employee>
+            employees, uint minAge, uint maxAge) =>
+                employees.Where(e => (e.Age >= minAge && e.Age <= maxAge));
+
+        public static IQueryable<Employee> Search(this IQueryable<Employee> employees,
+            string searchTerm)
+            {
+                if (string.IsNullOrWhiteSpace(searchTerm))
+                    return employees;
+
+                var lowerCaseTerm = searchTerm.Trim().ToLower();
+                return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+            }
+            
+
     }
 }
--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124f6a84ec3570bb47f87b485b90db151372d132")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e64f07a40e838f11decf42939df0dd331ec550d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

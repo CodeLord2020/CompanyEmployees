c6fb2fd0c7ffc5e7a20263bc5f84f00e01f5a0a3
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-27 10:20:06 +0100

    age filters to emp

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -7,6 +7,8 @@ namespace Shared.RequestFeatures
 {
     public class EmployeeParameters : RequestParameters
     {
-        
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set;}
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }

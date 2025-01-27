f4bf2c84317257e5e5130942d3657dc83975b59b
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-27 12:29:55 +0100

    MaxAgeRangeBadRequestException

--- Entities/Exceptions/MaxAgeRangeBadRequestException.cs
+++ Entities/Exceptions/MaxAgeRangeBadRequestException.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Exceptions
 {
-    public class MaxAgeRangeBadRequestException : BadRequestException
+    public sealed class MaxAgeRangeBadRequestException : BadRequestException
     {
         public MaxAgeRangeBadRequestException() 
         : base("Max age can't be less than min age.")

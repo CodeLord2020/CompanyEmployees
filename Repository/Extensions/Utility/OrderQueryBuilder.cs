137364451d039469cbb0f40f4d9b16841c34a793
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-27 11:30:45 +0100

    add OrderQueryBuilder logic

--- Repository/Extensions/Utility/OrderQueryBuilder.cs
+++ Repository/Extensions/Utility/OrderQueryBuilder.cs
@@ -5,8 +5,34 @@ using System.Threading.Tasks;
 
 namespace Repository.Extensions.Utility
 {
-    public class OrderQueryBuilder
+    public static class OrderQueryBuilder
     {
-        
+        public static string CreateOrderQuery<T>(string orderByQueryString)
+        {
+            var OrderbySplit = orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(T).GetProperties(BindingFlags.Public |
+                                BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in OrderbySplit)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi =>
+                    pi.Name.Equals(propertyFromQueryName,
+                     StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty == null)
+                    continue;
+
+                    var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+                    orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            return orderQuery;
+        }
     }
 }

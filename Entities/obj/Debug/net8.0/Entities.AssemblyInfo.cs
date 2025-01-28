e22340336b94317900bc941d5d3f8c5f608f73bc
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-28 09:49:09 +0100

    User model xend

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33570b6840b9607615a22e8ae03f2557279c8b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ead7bb91826e4c8d1a334083a97bc60fa603c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

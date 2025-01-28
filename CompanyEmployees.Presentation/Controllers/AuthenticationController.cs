0b988ecf1941b8452cbd7fc844213d05131246b4
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-28 10:34:06 +0100

    AuthenticationController cntlrs

--- CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
 
 namespace CompanyEmployees.Presentation.Controllers
 {
@@ -10,6 +11,9 @@ namespace CompanyEmployees.Presentation.Controllers
     [Route("api/[controller]")]
     public class AuthenticationController : ControllerBase
     {
-        
+        private readonly IServiceManager _service;
+        public AuthenticationController(IServiceManager service){
+            _service = service;
+        }
     }
 }

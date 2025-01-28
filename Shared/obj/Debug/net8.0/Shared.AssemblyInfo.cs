5fe8042802b62197508afa5e634b2b8e5a3c9159
Rasheed Babatunde <brasheed240@gmail.com>
2025-01-28 10:37:00 +0100

    UserForRegistrationDto

--- Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33570b6840b9607615a22e8ae03f2557279c8b2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b988ecf1941b8452cbd7fc844213d05131246b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

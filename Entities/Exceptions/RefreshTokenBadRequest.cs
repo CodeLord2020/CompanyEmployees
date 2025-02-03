f2f86982dabe3c22f56a630cbb769f95258535ce
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-03 17:13:43 +0100

    RefreshTokenBadRequest

---
+++ Entities/Exceptions/RefreshTokenBadRequest.cs
@@ -0,0 +1,7 @@
+public sealed class RefreshTokenBadRequest : BadRequestException
+{
+public RefreshTokenBadRequest()
+: base("Invalid client request. The tokenDto has some invalid values.")
+{
+}
+}
--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba27a20d6c62ed4eae33b86b3e83e8aea225e6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd3842d63710b2702375b4644973472b25d8933")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

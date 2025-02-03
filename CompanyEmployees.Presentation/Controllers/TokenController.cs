6024e17979b1c446510da52896fed148ed772264
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-03 17:09:47 +0100

    TokenController

--- CompanyEmployees.Presentation/Controllers/TokenController.cs
+++ CompanyEmployees.Presentation/Controllers/TokenController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
 
 namespace CompanyEmployees.Presentation.Controllers
 {
@@ -10,6 +11,7 @@ namespace CompanyEmployees.Presentation.Controllers
     [Route("api/[controller]")]
     public class TokenController : ControllerBase
     {
-        
+        private readonly IServiceManager _service;
+        public TokenController(IServiceManager service) => _service = service;
     }
 }

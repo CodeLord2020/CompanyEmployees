ca219ffd5fe67d1b1cbedb6c92c435ab6f66f7eb
Rasheed Babatunde <brasheed240@gmail.com>
2025-02-03 17:16:31 +0100

    R-TokenController

--- CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -51,5 +51,14 @@ namespace CompanyEmployees.Presentation.Controllers
 
             return Ok(tokenDto);
         }
+
+        [HttpPost("refresh")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> Refresh([FromBody]TokenDto tokenDto)
+        {
+            var tokenDtoToReturn = await
+            _service.AuthenticationService.RefreshToken(tokenDto);
+            return Ok(tokenDtoToReturn);
+        }
     }
 }
